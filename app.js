bb0ce86cb3d75f3965d52b98044bcfe513676135
Paweł Rybak <pawel.rybak13@gmail.com>
2022-09-24 15:13:42 +0200

    Changed background from canvas drawing to css

--- app.js
+++ app.js
@@ -5,8 +5,5 @@ const ctx = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-ctx.fillStyle = "#00000"
-ctx.fillRect(0, 0, canvas.width, canvas.height)
-
 wall = new Wall(50, 50, 150, 210)
 wall.draw("white")
