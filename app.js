3b8d5cc38172369851cb1b4ee3ec64b588d9c35a
Paweł Rybak <pawel.rybak13@gmail.com>
2022-09-24 17:09:24 +0200

    Added random wall coordinates generation

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const ctx = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-wall = new Wall(50, 50, 150, 210)
-wall.draw("white")
+const getRandomCoordinates = (marginFactor) => {
+    const horizontalLowerBound = canvas.width  * marginFactor
+    const verticalLowerBound = canvas.height * marginFactor
+    const horizontalUpperBound = canvas.width - horizontalLowerBound
+    const verticalUpperBound = canvas.height - verticalLowerBound
 
-console.log(wall.calculateLength());
+    return {
+        x1: Math.floor(Math.random() * (horizontalUpperBound - horizontalLowerBound) +  horizontalLowerBound),
+        y1: Math.floor(Math.random() * (verticalUpperBound - verticalLowerBound) + verticalLowerBound),
+        x2: Math.floor(Math.random() * (horizontalUpperBound - horizontalLowerBound) +  horizontalLowerBound),
+        y2: Math.floor(Math.random() * (verticalUpperBound - verticalLowerBound) + verticalLowerBound)
+    }
+}
+
+for (let i = 0; i < 8; i++) {
+    const coordinates = getRandomCoordinates(0.05)
+    wall = new Wall(coordinates.x1, coordinates.y1, coordinates.x2, coordinates.y2)
+    wall.draw("white")
+}
